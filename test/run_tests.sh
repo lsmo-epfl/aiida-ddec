1290cbe1d3d995e02955d7ad1047957243eee50d
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-08-24 00:24:29 +0200

    fix (to squash into the previous commit)

---
+++ test/run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Run precommit
+pre-commit run --all-files || ( git status --short ; git diff ; exit 1 )
+
+# Run pytest
+pytest ..
+
+# run single calculation tests
+
+#  run workflows
+
+# if all tests ran successfully
+echo "All tests have passed :-)"
