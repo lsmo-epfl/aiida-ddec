96f269dbff44b6fbf47b967b683965c4583a76ab
Daniele Ongari <daniele.ongari@gmail.com>
2019-08-23 09:53:33 +0200

    Removed tests/CI

--- aiida_ddec/tests/run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# <pre>/home/aiida/code/aiida-ddec/aiida_ddec/tests/
-# /home/aiida/code/aiida-ddec/aiida_ddec/tests/
-
-chown -R aiida /home
-/sbin/my_init
-su -c "export PATH=/home/aiida/.local/bin:$PATH  && verdi daemon status && py.test --verbose --junitxml=test-results.xml" aiida
