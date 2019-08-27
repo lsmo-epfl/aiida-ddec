ac23f02bbf0a3ea2ffe0d50374e8079a2efab210
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-08-27 10:07:10 +0200

    Remove unnecessary script add_codes.sh

--- .docker/my_init.d/add_codes.sh
+++
@@ -1,4 +0,0 @@
-# Install the ddec and cp2k codes
-DDEC_FOLDER=/home/aiida/code/aiida-ddec
-su aiida -c "verdi code show ddec@localhost || verdi code setup --config ${DDEC_FOLDER}/.docker/ddec-code.yml --non-interactive"
-su aiida -c "verdi code show cp2k@localhost || verdi code setup --config ${DDEC_FOLDER}/.docker/cp2k-code.yml --non-interactive"
