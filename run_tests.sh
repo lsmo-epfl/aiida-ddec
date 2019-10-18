6094659048002fbf613f0061210915a566241c3e
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-10-18 19:40:14 +0200

    Launch worchains from the run_tests.sh script

--- run_tests.sh
+++ run_tests.sh
@@ -10,9 +10,10 @@ pre-commit run --all-files || ( git status --short ; git diff ; exit 1 )
 # Run pytest
 pytest
 
-# run single calculation tests
 
-#  run workflows
+# run workflows
+verdi run examples/run_cp2k_ddec_h2o.py cp2k@localhost ddec@localhost  ~/code/chargemol_09_26_2017/atomic_densities/
+verdi run examples/run_cp2k_ddec_Zn-MOF-74.py cp2k@localhost ddec@localhost  ~/code/chargemol_09_26_2017/atomic_densities/
 
 # if all tests ran successfully
 echo "All tests have passed :-)"
