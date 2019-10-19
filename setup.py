ac68a027c30247b3cd510a944184f57a89bbb99b
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-10-19 15:30:07 +0200

    Work on setup.json and setup.py (#14)

--- setup.py
+++ setup.py
@@ -5,9 +5,18 @@ from __future__ import absolute_import
 import json
 from setuptools import setup, find_packages
 
-if __name__ == '__main__':
-    # Provide static information in setup.json
-    # such that it can be discovered automatically
+
+def run_setup():
     with open('setup.json', 'r') as info:
-        kwargs = json.load(info)  # pylint: disable=invalid-name
-    setup(packages=find_packages(), **kwargs)
+        kwargs = json.load(info)
+    setup(
+        include_package_data=True,
+        packages=find_packages(),
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        **kwargs
+    )
+
+
+if __name__ == '__main__':
+    run_setup()
