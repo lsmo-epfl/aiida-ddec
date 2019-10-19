0acfc30030ea6f53eea7970db801f21004597611
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2019-10-19 10:17:29 +0200

    Add travis and badges (#5)
    1) Set's up complete aiida environment
    2) Installs cp2k and ddec codes
    3) Runs pre-commint hooks
    4) Runs two workchains

--- tests/run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# <pre>/home/aiida/code/aiida-ddec/aiida_ddec/tests/
-# /home/aiida/code/aiida-ddec/aiida_ddec/tests/
-
-/sbin/my_init
-chown -R aiida /home
-su -c "export PATH=/home/aiida/.local/bin:$PATH && py.test --junitxml=test-results.xml" aiida
