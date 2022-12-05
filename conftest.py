4871ab3d0c6773edd001bf1335e5679a08c0ebbb
mpougin <72666131+mpougin@users.noreply.github.com>
2022-12-06 00:59:36 +0100

    fix pytest fixtures (#34)
    * fix pytest fixtures
    
    * fix pre-commit

--- conftest.py
+++ conftest.py
@@ -11,8 +11,6 @@ from examples import DATA_DIR as EXAMPLES_DATA_DIR
 
 from tests import DATA_DIR
 
-pytest_plugins = ['aiida.manage.tests.pytest_fixtures', 'aiida_testing.mock_code']  # pylint: disable=invalid-name
-
 
 @pytest.fixture(scope='function', autouse=True)
 def clear_database_auto(clear_database):  # pylint: disable=unused-argument
